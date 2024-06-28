91d2e7bdf79218da6c47d94d877dab3343db3244
Mogehs <153646093+Mogehs@users.noreply.github.com>
2024-06-28 17:02:47 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function playSong(track, pause = false) {
 }
 
 async function getSongs(folder) {
-  let a = await fetch(`https://main--mediaplayerr.netlify.app/${folder}/`);
+  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
   let response = await a.text();
   const infojs = await fetch(`http://127.0.0.1:5500/${folder}/info.json`);
   const jsonf = await infojs.json();
